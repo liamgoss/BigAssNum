9e13f97a016dec5607ef67e3bb2bac14773150fc
Liam Goss <50380089+liamgoss@users.noreply.github.com>
2023-03-13 11:18:58 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+// 174981883239023362650696299580 ^ 3 mod 174981883239023362450696299581 = 3280075090892117215879456260
+
+class BigAssNum {
+private:
+    vector<int> value;
+public:
+    void printValue() {
+        for (int i = 0; i < value.size(); i++) {
+            cout << value[i] << " ";
+        }
+        cout << endl;
+    }
+
+     
+    BigAssNum(string val) {
+        // Preferred constructor
+        for (auto it: val) {
+            value.push_back(atoi(&it)); // Convert digit character to int
+        }
+    }
+
+    BigAssNum() {
+        // Default Constructor
+        throw invalid_argument("No arguments specified!");
+    }
+
+
+};
+
+int main() {
+    BigAssNum test("01234");
+    test.printValue();
+    return 0;
+}
